00f323e1f452469499a62cd1b3c69a1375f45492
rakshit-singh2 <thisisjames007@gmail.com>
2026-01-02 13:15:35 +0530

    upto 34

--- sync_waitgroup/main.go
+++ sync_waitgroup/main.go
@@ -3,28 +3,45 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func worker(i int, wg *sync.WaitGroup) {
 	defer wg.Done() // Signal that this goroutine is done
 	fmt.Printf("worker %d started\n", i)
 	// some task is happening
+	time.Sleep(5 * time.Second)
 	fmt.Printf("worker %d end\n", i)
 }
 
-// worker(1)
-// worker(2)
-// worker(3)
+// func worker(i int) {
+
+// 	fmt.Printf("worker %d started\n", i)
+// 	// some task is happening
+// 	time.Sleep(5 * time.Second)
+// 	fmt.Printf("worker %d end\n", i)
+// }
 
 func main() {
 	// fmt.Println("Explore goroutine started")
 
 	var wg sync.WaitGroup
-	// Start 3 worker goroutines
-	for i := 1; i <= 3; i++ {
-		wg.Add(1) // Increment the WaitGroup counter
-		go worker(i, &wg)
-	}
+	wg.Add(1)
+	go worker(1, &wg)
+	wg.Add(1)
+	go worker(2, &wg)
+	wg.Add(1)
+	go worker(3, &wg)
+
+	// go worker(1)
+	// go worker(2)
+	// go worker(3)
+
+	// // Start 3 worker goroutines
+	// for i := 1; i <= 3; i++ {
+	// 	wg.Add(1) // Increment the WaitGroup counter
+	// 	go worker(i, &wg)
+	// }
 
 	// Wait for all workers to finish
 	wg.Wait()
